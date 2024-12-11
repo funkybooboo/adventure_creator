9d9fe13fba97f82c1d07f8f3890048ddedcc11c7
Nate Stott <nate.stott@pm.me>
2024-12-10 20:13:25 -0700

    set up libraries

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,24 +5,30 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            'plugin:prettier/recommended', // Add Prettier integration
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier: 'eslint-plugin-prettier', // Add Prettier plugin
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'prettier/prettier': ['error', { singleQuote: true, semi: true }], // Prettier rules
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
 )
